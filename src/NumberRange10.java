78ffebf981ab0d25d250e2090b9be161a9edacc2
tripti <triptilakhina9286@gmail.com>
2023-04-05 22:49:51 +0100

    1 - list of 10 numbers to print according to the user input range  - homework commit

--- src/NumberRange10.java
+++ src/NumberRange10.java
@@ -1,43 +1,54 @@
 import java.util.Scanner;
 
+// Writing a program using scanner class to print the numbers from the user input range of 10 numbers
 public class NumberRange10
 {
 
-    Scanner scanner=new Scanner(System.in);
+    // Creating object from scanner class to take the input
+    Scanner scanner = new Scanner(System.in);
 
 
-
-
-    public static void main(String[] args )
+    public static void main(String[] args)
     {
-        NumberRange10 numberRange10=new NumberRange10();
+        // Creating class object to call non-static method to static area
+        NumberRange10 numberRange10 = new NumberRange10();
+
+        // calling method using class object
         numberRange10.print10numbers();
 
     }
 
 
-
-
     public void print10numbers()
     {
-        System.out.println("Please write 2 numbers to provide a range of 10 numbers ex 23-33 pressing. Please press enter after each input. ");
 
+        // message to user for what to enter
+        System.out.println("Please write 2 numbers to provide a range of 10 numbers ex 1-10, pressing enter after each input. ");
+
+        // Storing variables using scanner object
         int rangenumber1 = scanner.nextInt();
         int rangenumber2 = scanner.nextInt();
 
 
-
-        for (int i = rangenumber1; (i <= rangenumber2) && (rangenumber1 + 10 == rangenumber2); i++)
+        // writing message to display to the user with the output only if the input range is of 10 numbers
+        if (rangenumber1 + 9 == rangenumber2)
         {
-            System.out.println(i);
+            System.out.println("\nHere is the list of all the numbers in your range " + rangenumber1 + "-" + rangenumber2 + ":");
 
+            // using forloop to get the list of numbers according to the user input range
+            for (int i = rangenumber1; (i <= rangenumber2) && (rangenumber1 + 9 == rangenumber2); i++)
+            {
+                System.out.println(i);
+
+            }
         }
-        if (rangenumber1 + 10 == rangenumber2)
+
+        // message to print if incorrect range is entered by the user
+        else
         {
-            System.out.println(" ");
-        } else {
             System.out.println("Invalid entry. Please enter a valid range of 10 numbers");
         }
 
     }
 }
+
